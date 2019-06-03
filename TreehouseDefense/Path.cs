0802908b413d1ee38853f393bb2b3163d0769d20
pr0t <prot@prot.com.pl>
2019-06-03 22:39:18 +0200

    PathTests added

---
+++ TreehouseDefense/Path.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    public class Path
+    {
+        private MapLocation[] pathLocations;
+
+        public Path(MapLocation[] pathLocations)
+        {
+            this.pathLocations = pathLocations;
+        }
+
+        public bool IsOnPath(MapLocation mapLocation)
+        {
+            int index = Array.FindIndex(pathLocations, (x) => x.X == mapLocation.X && x.Y == mapLocation.Y);
+
+            return index >= 0 ? true : false;
+        }
+    }
+}
