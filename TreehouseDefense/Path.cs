d327d049ab5f695a576f8ad6e8b981867b5418b4
pr0t <prot@prot.com.pl>
2019-06-20 22:38:25 +0200

    Modify Path.cs class.

--- TreehouseDefense/Path.cs
+++ TreehouseDefense/Path.cs
@@ -1,19 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace TreehouseDefense
 {
     public class Path
     {
-        private MapLocation[] pathLocations;
+        private MapLocation[] _path;
 
         public Path(MapLocation[] pathLocations)
         {
-            this.pathLocations = pathLocations;
+            this._path = pathLocations;
         }
 
-        public bool IsOnPath(MapLocation mapLocation)
+        public bool IsOnPath(MapLocation mapLocation) //=> _path.Contains(mapLocation);
         {
-            int index = Array.FindIndex(pathLocations, (x) => x.X == mapLocation.X && x.Y == mapLocation.Y);
+            int index = Array.FindIndex(_path, (x) => x.X == mapLocation.X && x.Y == mapLocation.Y);
 
             return index >= 0 ? true : false;
         }
